f43c8faf71a96dba3f8b9d87f4119574e491df70
arhan321 <arhanmcz@gmail.com>
2025-05-19 20:50:23 +0700

    freshh

---
+++ program/src/database/migrations/2025_05_19_204025_create_barangs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_barang')->unique();
+            $table->string('nama_barang');
+            $table->string('kategori');
+            $table->string('keterangan')->nullable();
+            $table->integer('stok')->default(0);
+            $table->integer('price')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
